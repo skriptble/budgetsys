ea13d004c98cf1e59f447d32e19efcdc83640066
Kris Brandow <kris.brandow@gmail.com>
2012-05-31 14:27:25 -0400

    Cleaning up budget value template and changing default values in form
    Updated the default values to pull from the current items in the edit form. Removed the first table and the broken edit button from the template

--- templates/budget_value.tpl.php
+++ templates/budget_value.tpl.php
@@ -1,24 +1,7 @@
 <?php
   $content = $element->content;
-  $aid = isset($element->aid) ? $aid = $element->aid : "";
+  $bvid = isset($element->bvid) ? $bvid = $element->bvid : "";
 ?>
-
-<?php if (user_is_logged_in()) : ?>
-  <p style="float: right;"><a href="?q=budget_value/<?php echo $aid; ?>/edit">Edit</a></p>
-  <?php endif; ?>
-  <?php echo render($content['title']); ?>
-    <table>
-      <tr>
-        <th>Account Number</th>
-        <th>Account Value</th>
-        <th>Account Year</th>
-      </tr>
-      <tr>
-        <td><?php echo render($content['account_number']); ?></td>
-        <td><?php echo render($content['value']); ?></td>
-        <td><?php echo render($content['year']); ?></td>
-      </tr>
-    </table>
     <table>
       <tr>
         <th>Account Number</th>
