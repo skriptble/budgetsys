a5ba5672737f391307cdbecf6c2c6a36bf943d7b
Kris Brandow <smileykrisproduction@gmail.com>
2012-10-21 12:14:05 -0400

    Adding hooks for budget value

---
+++ budgetsys_value/budgetsys_value.api.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * @file
+ * Hooks provided by the Budget Value module
+ */
+
+ /**
+ * Respond to updates to a budget value.
+ *
+ * This hook is invoked from budgetsys_value_save() after the budget value is updated in the budget value
+ * table in the database.
+ *
+ * @param $budget_value
+ *   The budget value that is being updated.
+ *
+ */
+function hook_budgetsys_value_update($budget_value) {
+  db_update('mytable')
+    ->fields(array('extra' => $budget_value))
+    ->execute();
+}
