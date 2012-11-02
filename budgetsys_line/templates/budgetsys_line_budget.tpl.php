02b43646ea22c23fad5cc9a1b5155ff19652380b
Kris Brandow <smileykrisproduction@gmail.com>
2012-11-02 19:10:17 -0400

    Upgrades to make the years system work using taxonomy.

--- budgetsys_line/templates/budgetsys_line_budget.tpl.php
+++ budgetsys_line/templates/budgetsys_line_budget.tpl.php
@@ -2,27 +2,7 @@
   $content = $element->content;
   $lid = isset($element->lid) ? $lid = $element->lid : "";
 ?>
-    <table>
-      <thead>
-        <th>Account Title</th>
-        <th>Account Number</th>
-        <th>Organization</th>
-        <th>Line Item Type</th>
-    <?php foreach($content['headers'] as $header) : ?>
-        <th><?php print $header; ?></th>   
-    <?php endforeach; ?>
-      </thead>
-      <tr>
-        <td><?php echo render($content['title']); ?></td>
-        <td><?php echo render($content['account_number']); ?></td>
-        <td><?php echo render($content['organization']); ?></td>
-        <td><?php echo render($content['line_item_type']); ?></td>
 
-      <?php foreach($content['values'] as $value) : ?>
-        <td><?php print render($value); ?></td>   
-      <?php endforeach; ?>          
-      </tr>      
-    </table>
     <table>
       <tr>
         <th>Account Title</th>
